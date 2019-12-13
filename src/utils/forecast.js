11f2e786ec11b4a3dddddb751d2506e43659c526
nspage <nicolas.s.page@gmail.com>
2019-12-13 23:05:38 +0100

    added max temp in daily predictions

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,6 +11,8 @@ const request = require('request')
 //    }  
 // })
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/e07c7c781a5fe1487cc3aecb29692641/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude)
     request({ url : url, json : true}, (error, {body}) => {
@@ -19,7 +21,7 @@ const forecast = (latitude, longitude, callback) => {
            } else if (body.error) {
             callback('Unable to find location', undefined)
            } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. The max temperature today will be ' + body.daily.data[0].temperatureHigh+' degrees.')
            } 
     })
 }
